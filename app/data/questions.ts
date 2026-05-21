1a89d0e7977a5648297778db00598574153dfa3e
Ingrid <ingridstanic@gmail.com>
2026-05-21 09:16:23 +0200

    created type and data for questions

---
+++ app/data/questions.ts
@@ -0,0 +1,80 @@
+import { Question } from "../models/Question";
+
+export const questions: Question[] = [
+  {
+    question: "Vad heter Meredith Greys mamma?",
+    options: ["Catherine Avery", "Adele Webber", "Ellis Grey", "Diane Pierce"],
+    correctAnswerIndex: 2,
+  },
+  {
+    question: `Vem kallas för "McDreamy"?`,
+    options: ["Derek Shepard", "Mark Sloan", "Owen Hunt", "Jackson Avery"],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: "Vad heter sjukhuset i början?",
+    options: [
+      "Grey Sloan Memorial Hospital",
+      "Seattle Grace Hospital",
+      "Mercy West Hospital",
+      "Oceanside Wellness Hospital",
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Vem gifter sig Cristina Yang med?",
+    options: ["Owen Hunt", "Preston Burke", "Shane Ross", "Mark Sloan"],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: `Vem är känd för repliken "It´s a beautiful day to save lives"?`,
+    options: [
+      "Richard Webber",
+      "Amelia Shepard",
+      "Derek Shepard",
+      "Miranda Bailey",
+    ],
+    correctAnswerIndex: 2,
+  },
+  {
+    question:
+      "Vem var det som egentligen amputerade Arizonas ben efter flygolyckan?",
+    options: ["Callie Torres", "Owen Hunt", "Richard Webber", "Alex Karev"],
+    correctAnswerIndex: 3,
+  },
+  {
+    question: "Vad döptes Seattle Grace Hospital om till efter flygolyckan?",
+    options: [
+      "Grey Seattle Hospital",
+      "Mercy West Memorial Hospital",
+      "Grey Sloan Memorial Hospital",
+      "Seattle Grace Mercy Death Hospital",
+    ],
+    correctAnswerIndex: 2,
+  },
+  {
+    question: "Vem är biologisk pappa till Maggie Pierce?",
+    options: [
+      "Thatcher Grey",
+      "Derek Shepard",
+      "Preston Burke",
+      "Richard Webber",
+    ],
+    correctAnswerIndex: 3,
+  },
+  {
+    question: "Vilken diagnos får Adele Webber i serien?",
+    options: ["ALS", "Alzheimers", "Parkinsons", "Huntingtons"],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Varför manipulerar Meredith Dereks Alzheimers studie?",
+    options: [
+      "För att Cristina ber henne göra det",
+      "För en patients skull",
+      "För att skydda en kollega",
+      "För att ge Adele Webber den riktiga medicinen istället för placebo",
+    ],
+    correctAnswerIndex: 3,
+  },
+];
